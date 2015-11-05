e559db6df3659ecec478e12d0c36f8ed7c42439d
Truong Tran <truongtran.cs@gmail.com>
2015-11-05 20:50:22 +0700

    init Spring-boot

---
+++ src/main/java/vn/edu/hcmut/emrre/spring/Application.java
@@ -0,0 +1,21 @@
+package vn.edu.hcmut.emrre.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String... args) {
+        ApplicationContext appContext = SpringApplication.run(Application.class, args);
+
+        // ContactService contactService =
+        // appContext.getBean(ContactService.class);
+        // System.out.println(contactService.ping());
+        System.out.println("hello");
+
+        SpringApplication.exit(appContext);
+    }
+}
---
+++ src/main/java/vn/edu/hcmut/emrre/spring/controller/HomeController.java
@@ -0,0 +1,15 @@
+package vn.edu.hcmut.emrre.spring.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/")
+public class HomeController {
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(){
+        return "home";
+    }    
+}
