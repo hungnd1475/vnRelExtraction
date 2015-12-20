6e34b72ce75ff36be6b87b475d92b7c845b53b87
Truong Tran <truongtran.cs@gmail.com>
2015-12-20 21:11:17 +0700

    complete

--- src/main/java/vn/edu/hcmut/emrre/core/entity/record/App.java
+++ src/main/java/vn/edu/hcmut/emrre/core/entity/record/App.java
@@ -1,17 +1,8 @@
 package vn.edu.hcmut.emrre.core.entity.record;
 
-import java.util.List;
-import java.util.Properties;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.semgraph.SemanticGraph;
-import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
-import edu.stanford.nlp.util.CoreMap;
 import vn.edu.hcmut.emrre.core.entity.sentence.Sentence;
 import vn.edu.hcmut.emrre.core.entity.sentence.SentenceDAO;
 import vn.edu.hcmut.emrre.core.entity.sentence.SentenceDAOImpl;
